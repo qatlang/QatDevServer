9b2db1d4f1175a4515dcd5e2aa98033aae80dd61
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-09 18:53:10 +0530

    Added models to support `compile` endpoint

--- models.go
+++ models.go
@@ -1,1 +1,35 @@
 package main
+
+type NewCompileFile struct {
+	Content string `json:"content"`
+	Time    string `json:"time"`
+}
+
+type FilePos struct {
+	Line int `json:"line"`
+	Char int `json:"char"`
+}
+
+type FileRange struct {
+	File  string  `json:"file"`
+	Start FilePos `json:"start"`
+	End   FilePos `json:"end"`
+}
+
+type Problem struct {
+	IsError bool      `json:"isError"`
+	Message string    `json:"message"`
+	Range   FileRange `json:"range"`
+}
+
+type SystemCompileResult struct {
+	Problems  []Problem `json:"problems"`
+	Status    bool      `json:"status"`
+	QatTime   int       `json:"qatTime"`
+	ClangTime int       `json:"clangTime"`
+	HasMain   bool      `json:"hasMain"`
+}
+
+type CompileStatusFail struct {
+	Status string `json:"status"`
+}
