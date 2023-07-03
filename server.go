e18d8ebd5366f40801a1f1cd4f3ffb873450c202
Aldrin Mathew <admin@aldrinmathew.com>
2023-07-03 17:45:37 +0200

    Added logic for updating the wakatime tokens
    - Previously the tokens for wakatime authentication were set and
      updated manually
    
    - Now it is automatically updated by checking occasionally and if the
      expiration is close, the refresh token is used to get the new tokens

--- server.go
+++ server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func main() {
@@ -23,6 +28,83 @@ func main() {
 	r := mux.NewRouter()
 	var collections Collections
 	ConnectDB(&collections)
+	dur := time.Duration(4 * time.Hour)
+	periodicChannel := time.Tick(dur)
+	go func() {
+		for {
+			<-periodicChannel
+			res := collections.Config.FindOne(context.Background(), bson.M{})
+			if res == nil {
+				log.Fatalf("Could not retrieve server config")
+			}
+			var config ServerConfig
+			err = res.Decode(&config)
+			if err != nil {
+				log.Fatalf("Error occured while decoding the server config")
+			}
+			wakatimeExpiry, err := time.Parse(time.RFC3339, config.Wakatime.ExpiresAt)
+			if err != nil {
+				diff := time.Until(wakatimeExpiry)
+				if diff.Hours() < 24.0 {
+					reqData := url.Values{
+						"client_id":     {config.Wakatime.ClientID},
+						"client_secret": {config.Wakatime.ClientSecret},
+						"redirect_uri":  {"https://qat.dev"},
+						"refresh_token": {config.Wakatime.RefreshToken},
+						"grant_type":    {"refresh_token"},
+					}
+					resp, err := http.PostForm(config.Wakatime.RefreshURL, reqData)
+					if err != nil {
+						log.Fatalf("Error occured while refreshing the Wakatime token")
+					}
+					var bodyBytes []byte
+					_, err = resp.Body.Read(bodyBytes)
+					if err != nil {
+						log.Fatalf("Error while reading the bytes of the response of refreshing the Wakatime token")
+					}
+					bodyStr := string(bodyBytes)
+					if strings.Contains(bodyStr, "&") {
+						vals := strings.Split(bodyStr, "&")
+						for i := 0; i < len(vals); i++ {
+							if strings.Contains(vals[i], "=") {
+								itemSplit := strings.Split(vals[i], "=")
+								switch itemSplit[0] {
+								case "access_token":
+									config.Wakatime.AccessToken = itemSplit[1]
+								case "refresh_token":
+									config.Wakatime.RefreshToken = itemSplit[1]
+								case "expires_at":
+									{
+										expStr, err := url.QueryUnescape(itemSplit[1])
+										if err != nil {
+											log.Fatalf("Error while decoding token expiry date string")
+										}
+										config.Wakatime.ExpiresAt = expStr
+									}
+								}
+							} else {
+								log.Fatalf("Error parsing item at index %d in the response of refreshing the wakatime token", i)
+							}
+						}
+						updateRes, err := collections.Config.UpdateOne(context.Background(), bson.M{},
+							bson.M{"$set": bson.M{
+								"wakatime.accessToken":  config.Wakatime.AccessToken,
+								"wakatime.refreshToken": config.Wakatime.RefreshToken,
+								"wakatime.expiresAt":    config.Wakatime.ExpiresAt}})
+						if err != nil {
+							log.Fatalf("Error while updating Wakatime configuration")
+						}
+						if updateRes.ModifiedCount != 1 {
+							log.Fatalf("Updating Wakatime configuration failed")
+						}
+					} else {
+						log.Fatalf("Error while parsing the response for refreshing Wakatime token")
+					}
+				}
+			}
+
+		}
+	}()
 	if len(os.Args) != 2 {
 		os.RemoveAll(os.Getenv("COMPILE_DIR"))
 	} else {
